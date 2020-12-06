9bc0d00f051292a4a536f7f384d02baaf07b3310
pochlax <rafa3l389@gmail.com>
2020-12-07 05:22:29 +0700

    Connected to MySQL

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
-print('Hello, world!')
+import mysql.connector
+login_db = mysql.connector.connect(host = 'localhost', user = 'root', passwd = 'password123', database = 'LoginCombos')
 
-print("I'm trying to commit")
+print (login_db)
 
-print ("Testing")
+if (login_db):
+    print("connection successful")
+else:
+    print("connection failed")
+
+cur = login_db.cursor()
+
+cur.execute("SELECT * FROM userPass")
+
+users = [] 
+passwords = []
+
+for row in cur.fetchall():
+    users.append(row[1])
+    passwords.append(row[2])
+
+print("Enter the Username:")
+userInput = input()
+
+print("Enter the Password:")
+passwordInput = input()
+
+index = 0
+
+for u in users:
+    if (userInput == u):
+        break
+    else:
+        index = index + 1
+
+if (userInput in users):
+    if (passwordInput == passwords[index]):
+        print("Combination Correct")
+
+login_db.close()
